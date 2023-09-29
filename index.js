bf3b1eea946cb3b96c09091668de90808d29195a
shubham1upadhyay <shubhamup47@gmail.com>
2023-09-29 10:47:09 +0530

    code upadted

--- index.js
+++ index.js
@@ -80,8 +80,8 @@
 
         `; 
         }
-        // container.appendChild(item);
-        // r_container.appendChild(r_item);
+        container.appendChild(item);
+        r_container.appendChild(r_item);
         live_container.appendChild(live_item);
     }
     
@@ -144,3 +144,26 @@ more.addEventListener('click', ()=>{
 share.addEventListener('click', ()=>{
     alert("Under Construction, Will be updated soon !");
 })
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
